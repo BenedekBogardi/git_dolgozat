909907299d7772a30a6b3f708c31bc75a16f74cc
Petrik Diak <diak@petrik.hu>
2023-12-18 12:48:39 +0100

    masodik_feladat

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-if(document.getElementById("gomb").clicked == true)
-{
-    szamok = 1;
-    szamok = document.getElementById("bemenet");
-    console.log(szamok);
+let szamok = []
+function szamInput() {
+    var szam = document.getElementById("bemenet").value;
+    szamok.push(szam);
+    console.log(szam);
 }
